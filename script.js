f9035b961325710196d0f7c580158c16861515e1
gutochiele <63303276+gutochiele@users.noreply.github.com>
2024-02-09 16:30:05 +0100

    changed the code to make a cell based matrix

--- script.js
+++ script.js
@@ -1,55 +1,22 @@
-const gameboard = {
-    matrix: [
-        [0, 0, 0], 
-        [0, 0, 0], 
-        [0, 0, 0]
-    ]
-}
-
-function getPlayers(playerOne, playerTwo){
-    return{
-        playerOne: playerOne,
-        playerTwo: playerTwo
+const createBoard = function(){
+    return function(){
+        let matrix = [''];
+        for (let i = 0; i < 9; i++) {
+            matrix[i] = ['']
+        }
+        return matrix;  //remember a function must return something
     }
-}
-
-if (
-    // Rows
-    ((gameboard.matrix[0][0] === 1) && (gameboard.matrix[0][1] === 1) && (gameboard.matrix[0][2] === 1)) ||
-    ((gameboard.matrix[1][0] === 1) && (gameboard.matrix[1][1] === 1) && (gameboard.matrix[1][2] === 1)) ||
-    ((gameboard.matrix[2][0] === 1) && (gameboard.matrix[2][1] === 1) && (gameboard.matrix[2][2] === 1)) ||
-    // Columns
-    ((gameboard.matrix[0][0] === 1) && (gameboard.matrix[1][0] === 1) && (gameboard.matrix[2][0] === 1)) ||
-    ((gameboard.matrix[0][1] === 1) && (gameboard.matrix[1][1] === 1) && (gameboard.matrix[2][1] === 1)) ||
-    ((gameboard.matrix[0][2] === 1) && (gameboard.matrix[1][2] === 1) && (gameboard.matrix[2][2] === 1)) ||
-    // Diagonals
-    ((gameboard.matrix[0][0] === 1) && (gameboard.matrix[1][1] === 1) && (gameboard.matrix[2][2] === 1)) ||
-    ((gameboard.matrix[0][2] === 1) && (gameboard.matrix[1][1] === 1) && (gameboard.matrix[2][0] === 1))
-) {
-    console.log("Win X condition met");
-}
-
-if (
-    // Rows
-    ((gameboard.matrix[0][0] === 2) && (gameboard.matrix[0][1] === 2) && (gameboard.matrix[0][2] === 2)) ||
-    ((gameboard.matrix[1][0] === 2) && (gameboard.matrix[1][1] === 2) && (gameboard.matrix[1][2] === 2)) ||
-    ((gameboard.matrix[2][0] === 2) && (gameboard.matrix[2][1] === 2) && (gameboard.matrix[2][2] === 2)) ||
-    // Columns
-    ((gameboard.matrix[0][0] === 2) && (gameboard.matrix[1][0] === 2) && (gameboard.matrix[2][0] === 2)) ||
-    ((gameboard.matrix[0][1] === 2) && (gameboard.matrix[1][1] === 2) && (gameboard.matrix[2][1] === 2)) ||
-    ((gameboard.matrix[0][2] === 2) && (gameboard.matrix[1][2] === 2) && (gameboard.matrix[2][2] === 2)) ||
-    // Diagonals
-    ((gameboard.matrix[0][0] === 2) && (gameboard.matrix[1][1] === 2) && (gameboard.matrix[2][2] === 2)) ||
-    ((gameboard.matrix[0][2] === 2) && (gameboard.matrix[1][1] === 2) && (gameboard.matrix[2][0] === 2))
-) {
-    console.log("Win O condition met");
-}
+}();
+const gameboard = createBoard();
 
 
-
-
-const players = getPlayers('Gustavo', 'Roberto')
-
-console.log('Player 1: ' + players.playerOne);
-console.log('Player 2: ' + players.playerTwo);
-console.log(gameboard.matrix.join('\n'))
+const winCombo = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
